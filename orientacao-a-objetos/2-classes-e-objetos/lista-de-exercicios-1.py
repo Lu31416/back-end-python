38b00d3e7bea174f75c1e36f56a4a14c275136d9
Luisa Tutau <58300331+Lu31416@users.noreply.github.com>
2023-09-01 16:48:59 -0300

    .

--- orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -1,5 +1,27 @@
 # Crie uma classe que modele o objeto "carro".
-Carro 
+class Carro:
+    def __init__(self,modelo,cor):
+        self.ligado = False
+        self.velocidade = 0
+        self.modelo = modelo 
+        self.cor = cor
+
+    def ligar(self):
+        self.ligado = True
+        print ('o carro esta ligado')
+    
+    def desliga(self):
+        self.ligado = False
+        print ('o carro esta desligado')  
+
+    def status(self):
+        print(f"Modelo: {self.modelo}")
+        print(f"Cor: {self.cor}")
+
+meu_carro = Carro("Corolla", "Prata")
+meu_carro.ligar()
+meu_carro.status()
+
 # Um carro tem os seguintes atributos: ligado, cor, modelo, velocidade.
 # Um carro tem os seguintes comportamentos: liga, desliga, acelera, desacelera.
 
