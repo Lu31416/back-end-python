7fc4ec1421483c195f81278786ce45b41324d443
Luisa Tutau <58300331+Lu31416@users.noreply.github.com>
2023-09-04 11:14:11 -0300

    ok

--- orientacao-a-objetos/7-classes-abstratas/lista-de-exercicios-2.py
+++ orientacao-a-objetos/7-classes-abstratas/lista-de-exercicios-2.py
@@ -13,37 +13,64 @@
 
 # Para modelar seu sistema, utilize obrigatoriamente os conceitos "classe", "herança", "propriedades", "encapsulamento" e "classe abstrata".
 class Cliente:
-    def __init__(self, nome, telefone, renda_mensal):
-    self.nome = nome
-    self.telefone = telefone
-    self.renda_mensal = renda_mensal
+    def __init__(self, nome, telefone, renda_mensal, genero):
+        self.nome = nome
+        self.telefone = telefone
+        self.renda_mensal = renda_mensal
+        self.genero = genero
 
 
-class ClienteMulher(cliente):
+class ClienteMulher(Cliente):
     def __init__(self, nome, telefone, renda_mensal):
-        super().__init__(nome, telefone, renda_mensal)
+        super().__init__(nome, telefone, renda_mensal, genero="mulher")
         self.cheque_especial = renda_mensal
      
 
 class Conta:
-    def __init_(self):
-    self.titulares = []
-    self.saldo = 0
+    def __init__(self, titulares):
+        self.titulares = titulares
+        self.saldo = 0
+        self.operacoes = []
 
-    def saque(self):
+    def saque(self, valor):
         for titular in self.titulares:
-            if titular.mulher:
-                saldo_disponivel = self.saldo + cheque_especial
-            else
-        if saldo > 0
-            saldo -= extracao
-            print ('voce extraiu {extracao}, seu novo saldo é{saldo}')
-        else print ()
+            if titular.genero == "mulher":
+                saldo_disponivel = self.saldo + titular.cheque_especial
+            else:
+                saldo_disponivel = self.saldo
+
+        if valor <= saldo_disponivel:
+            self.saldo -= valor
+            self.operacoes.append(f"saque: -{valor}")
+            print (f'saque de {valor}realizado com sucesso, novo saldo {self.saldo} ')
+            return
+        print('saldo insuficiente')
+
+    def deposito(self, valor):
+        self.saldo += valor
+        self.operacoes.append(f'deposito: +{valor}')
+        print (f'deposito de {valor}realizado com sucesso, novo saldo {self.saldo} ')
+
+    def extrato(self):
+        print(f'saldo atual: {self.saldo}')
+        print('operacoes')
+        for operacao in self.operacoes:
+            print(operacao)
 
 
+cliente1 = ClienteMulher("Maria", "123456789", 3000)
+cliente2 = Cliente("João", "987654321", 4000, genero="homem")
 
+conta = Conta([cliente1, cliente2])
+
+conta.deposito(1000)
+conta.saque(500)
+conta.extrato()
+
+'''
 contas devem ter um ou mais clientes
 clientes: nome, telefone, renda mensal
 conta: saldo, operacoes: saque: dimunie saldo, deposito, aumenta o saldo
 cliente mulher pode fazer saque ate -renda mensal
 cliente homem nao pode
+'''
